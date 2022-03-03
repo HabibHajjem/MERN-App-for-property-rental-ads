b8edc36be39569024af2c80be89634cf2f9c9e71
HabibHajjem <hajjemhabib62@gmail.com>
2022-03-03 09:16:02 +0100

    preparing for deployement

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { path } = require('express/lib/application');
+const path = require('path')
 const connectDb = require('./config/connectDb');
 require('dotenv').config();
 const authRoute = require('./routes/auth');
