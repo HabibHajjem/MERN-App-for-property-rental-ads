dfbfd4c1019ef9b0bb823f7ff354544eb3afa27f
HabibHajjem <hajjemhabib62@gmail.com>
2022-03-07 14:55:47 +0100

    changing multer destination

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: 'https://hungry-nobel-f450ba.netlify.app/public/uploads' ,
+    destination: 'https://hungry-nobel-f450ba.netlify.app/uploads' ,
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
