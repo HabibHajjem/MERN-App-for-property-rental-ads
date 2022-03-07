f8184db04749174e9276b8f2c98c05b34bc8966c
HabibHajjem <hajjemhabib62@gmail.com>
2022-03-07 16:48:27 +0100

    adding script to packagejson

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors')
 const path = require('path')
-const fs = require('fs')
 const connectDb = require('./config/connectDb');
 require('dotenv').config();
 const authRoute = require('./routes/auth');
