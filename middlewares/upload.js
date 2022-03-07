4119718200336f744cf23b9df8cb258590bb7914
HabibHajjem <hajjemhabib62@gmail.com>
2022-03-07 15:55:43 +0100

    changing multer destination

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: 'build/public/uploads' ,
+    destination: 'client/build/public/uploads' ,
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
