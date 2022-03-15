e53a854faac135cb5c6306c9f556f360b962d8e5
HabibHajjem <hajjemhabib62@gmail.com>
2022-03-15 15:06:55 +0100

    update

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const postRoute = require('./routes/post');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// const corsOptions ={
-//     origin:'*', 
-//     credentials:true,            
-//     optionSuccessStatus:200,
-// }
-// app.use(cors(corsOptions)) 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            
+    optionSuccessStatus:200,
+}
+app.use(cors(corsOptions)) 
 
 app.use(express.json());
 app.use('/api',authRoute);
